8dc0625c391fa880db2bd422d67ca4883911e73f
Daijoubu22 <alextrio.2224@gmail.com>
2023-04-06 17:19:47 +0300

    Init frontend

---
+++ frontend/src/pages/HomePage/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TestModule } from '@modules/TestModule';
+
+function HomePage() {
+    return (
+        <div>
+            <TestModule />
+        </div>
+    );
+}
+
+export default HomePage;
